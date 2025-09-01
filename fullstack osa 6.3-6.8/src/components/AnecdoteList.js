d8b247dda019fb8b3851231dd0fa276b237f0478
Ratchat1918 <160712854+Ratchat1918@users.noreply.github.com>
2025-09-01 19:22:02 +0300

    Add files via upload

---
+++ fullstack osa 6.3-6.8/src/components/AnecdoteList.js
@@ -0,0 +1,31 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { voteFor } from './reducers/anecdoteReducer'
+
+const AnecdoteList=()=>{
+    let anecdotes = useSelector(state => state)
+      anecdotes = anecdotes.sort((a, b) => b.votes - a.votes)
+      const dispatch = useDispatch()
+    
+      const vote = (id) => {
+        dispatch(voteFor(id))
+        console.log('vote', id)
+        console.log(anecdotes)
+      }
+    return(
+        <div>
+            {anecdotes.map(anecdote => 
+            <div key={anecdote.id}>
+                <div>
+                    {anecdote.content}
+                </div>
+            <div>
+                has {anecdote.votes}
+                <button onClick={() => vote(anecdote.id)}>vote</button>
+            </div>
+            </div>
+        )}
+        </div>
+    )
+}
+
+export default AnecdoteList

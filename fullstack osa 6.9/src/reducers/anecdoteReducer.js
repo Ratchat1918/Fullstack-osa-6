e110bbf759389763f2d53f1baa4e73a499e55781
Ratchat1918 <160712854+Ratchat1918@users.noreply.github.com>
2025-09-05 19:34:29 +0300

    Add files via upload

---
+++ fullstack osa 6.9/src/reducers/anecdoteReducer.js
@@ -0,0 +1,66 @@
+import { createSlice } from '@reduxjs/toolkit'
+const anecdotesAtStart = {anecdotes:[
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+}
+
+const getId = () => (100000 * Math.random()).toFixed(0)
+const getVotes = () => Number((10 * Math.random()).toFixed(0))
+
+const asObject = (anecdote) => {
+  return {
+    content: anecdote,
+    id: getId(),
+    votes: getVotes()
+  }
+}
+
+const initialState = anecdotesAtStart.anecdotes.map(asObject)
+
+
+const anecdoteReducer = (state = initialState, action) => {
+  if(action.type==='ADD'){
+    const id=action.payload.id
+    const voteToChange=state.find(n=>n.id===id)
+    const newVote=voteToChange.votes+1
+    const changedVote={
+      ...voteToChange,
+      votes:newVote
+    }
+    return state.map(vote =>
+        vote.id !== id ? vote : changedVote 
+      )
+  }
+  else if(action.type==="NEW_ANECDOTE"){
+    const newAnecdote={
+      content:action.payload.content,
+      id:action.payload.id,
+      votes:0
+    }
+    return state.concat(newAnecdote)
+  }
+  return state
+}
+export const voteFor = (id) =>{
+    return{
+      type:"ADD",
+      payload:{id}
+    }
+  }
+export const createAnecdote = (content) => {
+    return {
+      type: 'NEW_ANECDOTE',
+      payload: {
+        content: content,
+        id: getId(),
+        votes: 0
+      }
+    }
+  }
+
+export default anecdoteReducer
